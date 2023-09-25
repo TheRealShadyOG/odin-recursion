94816afa426497f38612ec4d95dbc0545455d352
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-25 08:29:43 -0500

    Add fibsRec() for recursion

--- fibonacci.js
+++ fibonacci.js
@@ -12,3 +12,17 @@ function fibs(num) {
   }
   return array;
 }
+
+// Using recursion
+function fibsRec(num) {
+  if (num === 1) {
+    return [0];
+  } else if (num === 2) {
+    return [0, 1];
+  } else {
+    return [
+      ...fibsRec(num - 1),
+      fibsRec(num - 1)[num - 2] + fibsRec(num - 1)[num - 3],
+    ];
+  }
+}
